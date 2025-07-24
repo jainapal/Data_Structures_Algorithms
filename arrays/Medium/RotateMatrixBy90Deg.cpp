d5cdc01fea1fcca3022fbba882acc7d631e1207b
Jaina Pal <jainapal5@gmail.com>
2025-07-24 10:20:17 +0530

    Rotate matrix by 90 deg done

---
+++ arrays/Medium/RotateMatrixBy90Deg.cpp
@@ -0,0 +1,29 @@
+// void rotateMatrix(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<vector<int>> temp(n, vector<int>(n, 0));
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < n;j++){
+//                 temp[j][n - i - 1] = matrix[i][j];
+//             }
+//         }
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < n;j++){
+//                 matrix[i][j] = temp[i][j];
+//             }
+//         }
+        
+//     }
+//     TC = O(N^2 + N^2), SC = O(N^2)
+---------------------------------------------------------------------
+// void rotateMatrix(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < i; j++){
+//                 swap(matrix[i][j], matrix[j][i]);
+//             }
+//         }
+//         for(int i = 0; i < n; i++){
+//             reverse(matrix[i].begin(), matrix[i].end());
+//         }
+// }
+// TC = O(N^2 + N^2), SC = O(1)
